c41b89f67188b594d623389421748d275c6da9d8
Darshan Jasani <167104440+Darshanjasani73@users.noreply.github.com>
2024-06-12 06:22:52 +0530

    Update PostCard.jsx

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const PostCard = ({ post }) => {
   return (
     <div className='group relative w-full border border-teal-500 hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[360px] transition-all'>
-      <Link to={`https://blogify-backend-1-0stb.onrender.com/post/${post.slug}`}>
+      <Link to={`/post/${post.slug}`}>
         <img
           src={post.image}
           alt="post cover"
@@ -16,7 +16,7 @@ const PostCard = ({ post }) => {
         <p className='text-lg font-semibold line-clamp-2'>{post.title}</p>
         <span className='italic text-sm'>{post.category}</span>
         <Link
-          to={`https://blogify-backend-1-0stb.onrender.com/post/${post.slug}`}
+          to={`/post/${post.slug}`}
           className='z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border border-teal-500 text-teal-500 hover:bg-teal-500 hover:text-white transition-all duration-300 text-center py-2 rounded-md !rounded-tl-none m-2'
         >
           Read article
