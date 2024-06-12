0438d3c63036aee5f2a6edb0788602f08c22c97f
Darshan Jasani <167104440+Darshanjasani73@users.noreply.github.com>
2024-06-12 05:46:13 +0530

    Update PostPage.jsx

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -15,7 +15,7 @@ const PostPage = () => {
         const fetchPost = async()=>{
             try {
                 setLoading(true)
-                const res = await fetch(`/api/post/getposts?slug=${postSlug}`)
+                const res = await fetch(`https://blogify-backend-1-0stb.onrender.com//api/post/getposts?slug=${postSlug}`)
                 const data = await res.json()
                 if(!res.ok){
                     setError(true)
