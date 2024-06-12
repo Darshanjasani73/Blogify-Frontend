f599533ad87ab78e5a85757330b3fd3c25be622a
Darshan Jasani <167104440+Darshanjasani73@users.noreply.github.com>
2024-06-12 06:16:13 +0530

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRoute = () => {
     const {currentUser} = useSelector((state)=>state.user)
-  return currentUser ? <Outlet /> : <Navigate to='https://blogify-backend-1-0stb.onrender.com/sign-in' />
+  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />
 }
 
 export default PrivateRoute
