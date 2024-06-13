a9f08b129d830056dc74db01ab05d4d4b44f9637
Darshan Jasani <167104440+Darshanjasani73@users.noreply.github.com>
2024-06-13 10:18:25 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const FooterCom = () => {
         <div className="grid w-full justify-between sm:flex md:grid-cols-1">
           <div className="mt-5">
             <Link
-              to="https://blogify-backend-1-0stb.onrender.com/"
+              to="/"
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
             >
               <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
